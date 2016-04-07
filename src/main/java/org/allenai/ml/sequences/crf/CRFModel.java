ead0e1cbb0b22f2afca174d1f9257a9324f149d4
Aria Haghighi <me@aria42.com>
2016-04-06 23:07:53 -0700

    Diff on CRFModel for debug

--- src/main/java/org/allenai/ml/sequences/crf/CRFModel.java
+++ src/main/java/org/allenai/ml/sequences/crf/CRFModel.java
@@ -6,6 +6,7 @@ import org.allenai.ml.sequences.SequenceTagger;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.val;
+import org.allenai.ml.util.Indexer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,6 +34,43 @@ public class CRFModel<S, O, F> implements SequenceTagger<S, O> {
     }
 
 
+    private static <F> List<String> diff(String prefix, Indexer<F> x, Indexer<F> y) {
+        List<String> res = new ArrayList<>();
+        if (x.size() != y.size()) {
+            res.add(prefix + " - size diff " + x.size() + " != " + y.size());
+        }
+        int n = Math.min(x.size(), y.size());
+        for (int idx=0; idx < n; ++idx) {
+            if (!x.get(idx).equals(y.get(idx))) {
+                res.add(prefix + " - elem diff " + idx + " "  + x.get(idx)  + " / "  + y.get(idx));
+            }
+        }
+        return res;
+    }
+
+    private static List<String> diff(String prefix, Vector x, Vector y) {
+        List<String> res = new ArrayList<>();
+        if (x.dimension() != y.dimension()) {
+            res.add(prefix + " - dim diff " + x.dimension() + " != " + y.dimension());
+        }
+        long n = Math.min(x.dimension(), y.dimension());
+        for (long idx=0; idx < n; ++idx) {
+            double xx = x.at(idx);
+            double yy = y.at(idx);
+            if (Math.abs(xx-yy) > 0.001) {
+                res.add(prefix + " - elem diff " + idx + " "  + x.at(idx)  + " / "  + y.at(idx));
+            }
+        }
+        return res;
+    }
+
+    public List<String> diff(CRFModel<S, O, F> otherModel) {
+        List<String> res = new ArrayList<>();
+        res.addAll(diff("nodeFeatures", this.featureEncoder.nodeFeatures, otherModel.featureEncoder.nodeFeatures));
+        res.addAll(diff("edgeFeatures", this.featureEncoder.nodeFeatures, otherModel.featureEncoder.nodeFeatures));
+        res.addAll(diff("weightDiffs", this.weights, otherModel.weights));
+        return res;
+    }
 
     @Override
     public List<S> bestGuess(List<O> input) {
