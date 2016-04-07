915f1e56a8e1e1ed6584803868b6262663e6f3d7
Aria Haghighi <me@aria42.com>
2016-04-07 11:20:06 -0700

    Remove diff

--- src/main/java/org/allenai/ml/sequences/crf/CRFModel.java
+++ src/main/java/org/allenai/ml/sequences/crf/CRFModel.java
@@ -33,45 +33,6 @@ public class CRFModel<S, O, F extends Comparable<F>> implements SequenceTagger<S
         return weights.copy();
     }
 
-
-    private static <F extends Comparable<F>> List<String> diff(String prefix, Indexer<F> x, Indexer<F> y) {
-        List<String> res = new ArrayList<>();
-        if (x.size() != y.size()) {
-            res.add(prefix + " - size diff " + x.size() + " != " + y.size());
-        }
-        int n = Math.min(x.size(), y.size());
-        for (int idx=0; idx < n; ++idx) {
-            if (!x.get(idx).equals(y.get(idx))) {
-                res.add(prefix + " - elem diff " + idx + " "  + x.get(idx)  + " / "  + y.get(idx));
-            }
-        }
-        return res;
-    }
-
-    private static List<String> diff(String prefix, Vector x, Vector y) {
-        List<String> res = new ArrayList<>();
-        if (x.dimension() != y.dimension()) {
-            res.add(prefix + " - dim diff " + x.dimension() + " != " + y.dimension());
-        }
-        long n = Math.min(x.dimension(), y.dimension());
-        for (long idx=0; idx < n; ++idx) {
-            double xx = x.at(idx);
-            double yy = y.at(idx);
-            if (Math.abs(xx-yy) > 0.001) {
-                res.add(prefix + " - elem diff " + idx + " "  + x.at(idx)  + " / "  + y.at(idx));
-            }
-        }
-        return res;
-    }
-
-    public List<String> diff(CRFModel<S, O, F> otherModel) {
-        List<String> res = new ArrayList<>();
-        res.addAll(diff("nodeFeatures", this.featureEncoder.nodeFeatures, otherModel.featureEncoder.nodeFeatures));
-        res.addAll(diff("edgeFeatures", this.featureEncoder.edgeFeatures, otherModel.featureEncoder.edgeFeatures));
-        res.addAll(diff("weightDiffs", this.weights, otherModel.weights));
-        return res;
-    }
-
     @Override
     public List<S> bestGuess(List<O> input) {
         if (input.size() < 2) {
